866d3e3008e290b1c1cac3f329112bb3a417bab7
kmohan96214 <kmohan96214@gmail.com>
2019-05-05 18:09:07 +0530

    express backend

---
+++ server.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const session = require('express-session');
+const redis = require('redis');
+const redisStore = require('connect-redis')(session);
+const client = redis.createClient();
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(session({
+    secret: 'ssshhhhh',
+    store: new redisStore({ host: 'localhost', port: 6379, client: client,ttl : 260}),
+    saveUninitialized: false,
+    resave: false
+}));
+
+//models
+const mUser = require('./models/musers').user;
+const mChat = require('./models/mchat').chat;
+
+app.use(bodyParser.json());
+
+var corsOption = {
+	orign: 'http://localhost:4200',
+	optionSuccessStatus: 200
+}
+
+app.use(cors(corsOption));
+
+
+//models
+const cat = mongoose.model('cat',{name:String});
+
+//db connection
+mongoose.connect('mongodb://localhost:27017/mean-book', {useNewUrlParser: true});
+
+app.listen(3000, () => {
+  console.log('Server started!');
+});
+
+/*APIs*/
+app.get("/api/testing_api", (req, res)=>{
+	res.send("Wow!!!! I am from Server :D");
+});
+//to create a new cat
+app.get("/api/save_kitty", (req, res)=>{
+	var ourKitty = new cat();
+	ourKitty.name = "Black Cat";
+	ourKitty.save((err)=>{
+		res.send(ourKitty);
+	});
+	
+});
+
+//to create user
+app.post('/api/create_user', (req, res)=>{
+	
+	const newUser = new mUser();
+	newUser.userName = req.body.userName;
+	newUser.email = req.body.email;
+	newUser.password = req.body.password;
+	newUser.age = req.body.age;
+	newUser.save((err)=>{
+		if(err)
+			res.status(500).json(err);
+		else
+			res.status(200).json(newUser);
+	})
+});
+
+//to do login user
+app.post("/api/do_login", (req, res)=>{
+	mUser.findOne({email:req.body.email, password: req.body.password},
+		{_id:1,userName:1},
+		(err, doc)=>{
+		if(err)
+			res.status(500).json(err);
+		else if(doc){
+			res.status(200).json(doc);
+		}
+		else
+			res.status(401).json({msg:"Invalid login details"});
+	});
+});
+
+//to do logout
+app.get("/api/do_logout",(req,res)=>{
+	res.status(200).json({msg:"logged out"});
+});
+
+//to create user
+app.post('/api/post_new_chat', (req, res)=>{
+
+	console.log(req.session);
+	//if(!req.session)
+	//	res.status(500).json({msg:"login first"})
+	//else
+	//{
+		const newChat = new mChat();
+		newChat.userName = req.body.userName;
+		newChat.userID = req.body.userID;
+		newChat.msg = req.body.msg;
+		console.log(newChat);
+		newChat.save((err)=>{
+			if(err)
+				res.status(500).json(err);
+			else
+				res.status(200).json(newChat);
+		})
+	//}
+});
+
+//to do latest chats
+app.get("/api/get_chats/:lastSynced", (req, res)=>{
+	if(!req.body)
+		res.status(500).json({msg:"login first"});
+
+	let query ={};
+	if(req.params.lastSynced != "null")
+		query={'crAt' : {
+			$gt:new Date().setTime(parseInt(req.params.lastSynced))}};
+
+	mChat.find(query,
+		{userName:1,msg:1,crAt:1},
+		{sort:{crAt: -1}},
+		(err, docs)=>{
+		if(err)
+			res.status(500).json(err);
+		else if(docs)
+			res.status(200).json(docs);
+
+	});
+});
